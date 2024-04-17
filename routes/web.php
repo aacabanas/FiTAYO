b83cc9aafbe0837bac89eda6812c5ef6fb5a1219
aacabanas <aacabanas2@student.apc.edu.ph>
2024-04-17 08:34:15 +0800

    Added routes, individual views for each corresponding route and starter template for each route(will update configuration later)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/register',function(){
+    return view('register');
+});
+Route::get('/account',function(){
+    return view('account');
 });
